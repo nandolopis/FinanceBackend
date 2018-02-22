584f54e74ae54f40b83320b79c4a52de82a98dea
Fernando Lopes <nandolopis@gmail.com>
2018-02-22 16:44:50 -0300

    Tratando erro do required

--- api/billingCycle/billingCycle.js
+++ api/billingCycle/billingCycle.js
@@ -4,13 +4,13 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true }
+    name: { type: String, required: [true, 'Informe um nome para lançamento'] },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor'] }
 })
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    name: { type: String, required: [true, 'Informe um nome para lançamento'] },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor'] },
     status: { type: String, required: false, uppercase: true, 
         enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
 })
@@ -23,4 +23,4 @@ const billingCycleSchema = new mongoose.Schema({
     debts: [debtSchema]
 })
 
-module.exports = restful.module('BillingCycle', billingCycleSchema)
+module.exports = restful.model('BillingCycle', billingCycleSchema)
--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
 const mongoose =  require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
 //module.exports = mongoose.connect('mongodb://ususario:senha@localhost:port/db_finance')
+
+//tratamento erro do required
+mongoose.Error.messages.general.required = "O atributo '{PATH}' É Obrigatório."
