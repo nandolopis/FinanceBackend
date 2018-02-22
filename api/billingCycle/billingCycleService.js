8c2d022f04eb593dc9cbe8b627d31ff60abbb083
Fernando Lopes <nandolopis@gmail.com>
2018-02-22 17:25:24 -0300

    habilitando as validações e trantando erros de put

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -2,7 +2,7 @@ const BillingCycle = require('./billingCycle.js')
 
 //criando serviços rast 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true})
+BillingCycle.updateOptions({new: true, runValidators: true})
 
 
 module.exports = BillingCycle
--- config/database.js
+++ config/database.js
@@ -2,5 +2,8 @@ const mongoose =  require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
 //module.exports = mongoose.connect('mongodb://ususario:senha@localhost:port/db_finance')
 
-//tratamento erro do required
+//tratamento erro
 mongoose.Error.messages.general.required = "O atributo '{PATH}' É Obrigatório."
+mongoose.Error.messages.Number.min = " O '{VALUE}' informado é menor que o limite minimo permitido, que é de '{MIN}'!"
+mongoose.Error.messages.Number.max = " O '{VALUE}' informado é menor que o limite maximo permitido, que é de '{MAX}'!"
+mongoose.Error.messages.String.enum = " O '{VALUE}' não é valido para o atributo '{PATH}'."
