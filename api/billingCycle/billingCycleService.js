55de43e1ee036e06084ddbddfc5ea8e5572b3987
Fernando Lopes <nandolopis@gmail.com>
2018-02-22 16:53:15 -0300

    Corrigino metodo put para retornar o resuldado na hora

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -2,6 +2,8 @@ const BillingCycle = require('./billingCycle.js')
 
 //criando serviços rast 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new: true})
+
 
 module.exports = BillingCycle
 
