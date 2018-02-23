2d65504e786f10667d4a71bc03a0dc76104ac433
Fernando Lopes <nandolopis@gmail.com>
2018-02-23 16:47:55 -0300

    valtado as dependencias antigas

--- api/billingSummary/billingSummaryService.js
+++ api/billingSummary/billingSummaryService.js
@@ -3,13 +3,13 @@ const BillingCycle = require('../billingCycle/billingCycle.js')
 
 //Mais uma funcao middleware
 function getSummary(req, res) {
-    BillingCycle.aggregate({
+    BillingCycle.aggregate([{
         $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
-    }, {
-        $group: { _id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
-    },{
-        $project: { _id: 0, credit: 1, debt: 1}
-    }, function(error, result){
+        }, {
+        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+        }, {
+        $project: {_id: 0, credit: 1, debt: 1}
+        }], function(error, result){
         if(error){
             res.status(500).json({errors: [error]})
         } else {
